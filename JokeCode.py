c6a28abb8e24726a5dc4a81624c938d5977dda52
Amar J B <myself.22555@gmail.com>
2020-04-11 11:07:17 +0530

    Added a JokeCode Python code on branch2

---
+++ JokeCode.py
@@ -0,0 +1,11 @@
+
+number1 = input("Enter the first number")
+number2 = input("Enter the second number")
+
+sum = number1 + nnumber2
+
+print("You thought I was gonna perform a magic trick? LMFAO!!")
+print("Here you go...")
+print("Just kidding man! XD")
+print(sum+"Yeah, that's it man, just the sum! -_-  ")
+
